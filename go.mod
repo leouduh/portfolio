c893574a71a3bf7ab65a5a0c5ade936819b827b3
leouduh <chigozieuduh.cu@gmail.com>
2026-08-10 19:05:28 +0100

    Feat: landing page done, we have something for now at least

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/leouduh/portfolio_website
+module github.com/leouduh/portfolio
 
 go 1.25.1
--- main.go
+++ main.go
@@ -1,1 +1,75 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"html/template"
+) 
+
+var homeTmpl = template.Must(template.ParseFiles("templates/layout.html", "templates/home.html"))
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	type Page struct{
+		Title string
+	}
+	data := Page{Title: "Home"}
+	err := homeTmpl.ExecuteTemplate(w, "base", data)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "temporary projects landing page")
+
+}
+
+func resumeHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "temporary resume landing page")
+
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "temporary contact landing page")
+}
+
+// write middle ware function to intercept trailing slashes
+// you pass the next function, which is the function that will run afte rthe middle ware function
+func stripTrailingSlash(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		// if block to handle if it has a trailing "/"
+		if len(r.URL.Path) > 1 && strings.HasSuffix(r.URL.Path, "/"){
+			http.Redirect(w, r, strings.TrimSuffix(r.URL.Path, "/"), http.StatusMovedPermanently)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main(){
+	port := os.Getenv("PORT")
+	if port == ""{
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/projects", projectsHandler)
+	mux.HandleFunc("/resume", resumeHandler)
+	mux.HandleFunc("/contact", contactHandler)
+
+	addr := ":" + port
+	log.Printf("Server starting on address %s", addr)
+
+	err := http.ListenAndServe(addr, stripTrailingSlash(mux))
+	if err != nil{
+		log.Fatalf("server failed: %v", err)
+	}
+
+}
