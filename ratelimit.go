f66177df0b5d8662518e5a1fbda9cad44b154285
leouduh <chigozieuduh.cu@gmail.com>
2026-08-26 12:36:21 +0100

    Feat: added rate limiting

---
+++ ratelimit.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type visitor struct {
+	limiter *rate.Limiter
+	lastSeen time.Time
+}
+
+var (
+	visitors = make(map[string]*visitor)
+	visitorsMu  sync.Mutex
+)
+
+func clientIP(r *http.Request) string {
+	// gets the user ip from their request
+	ip := r.Header.Get("X-Real-IP")
+	if ip != ""{
+		return ip
+	}
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+	return host
+}
+
+func getLimiter(ip string) *rate.Limiter {
+	visitorsMu.Lock()
+	defer visitorsMu.Unlock()
+
+	v, exists := visitors[ip] 
+	if !exists {
+		limiter := rate.NewLimiter(2, 10)
+		visitors[ip] = &visitor{
+			limiter: limiter,
+			lastSeen: time.Now(),
+
+		}
+		return limiter
+	}
+	v.lastSeen = time.Now()
+	return v.limiter
+
+}
+
+
+func cleanUpVisitors(){
+	for {
+		time.Sleep(time.Minute)
+		visitorsMu.Lock()
+		for ip, v := range visitors {
+			if time.Since(v.lastSeen) > 3*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		visitorsMu.Unlock()
+	}
+}
+
+func rateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		limiter := getLimiter(clientIP(r))
+		if !limiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+
+	})
+
+}
